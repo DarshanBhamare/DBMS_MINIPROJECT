7f65ffbfa7832f8245bc414ebc1abbd6904b9bac
Darshan Bhamare <bhamaredarshan023@gmail.com>
2025-10-28 07:58:21 +0530

    add backen url for deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,11 @@
+// import axios from "axios";
+
+// export const api = axios.create({
+//   baseURL: "http://localhost:5000/api"
+// });
+
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://dbms-miniproject-y9uw.onrender.com/api"
 });
